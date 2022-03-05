4cd86e1b73ae2efa6a0184c6747fa15866dbf81f
Thierry Arnoux <thierry.arnoux@gmx.net>
2022-03-05 20:12:09 +0800

    Store proof steps in a `metamath-knife` tree structure

--- metamath-mmp/src/worksheet.rs
+++ metamath-mmp/src/worksheet.rs
@@ -1,16 +1,21 @@
 use lazy_static::lazy_static;
 use log::*;
+use metamath_knife::diag::Diagnostic;
 use metamath_knife::formula::Formula;
 use metamath_knife::formula::Label;
 use metamath_knife::statement::as_str;
 use metamath_knife::statement::StatementAddress;
 use metamath_knife::statement::StatementRef;
 use metamath_knife::Database;
+use metamath_knife::tree::Tree;
+use metamath_knife::tree::NodeId;
 use regex::Regex;
 use ropey::Rope;
+use ropey::RopeSlice;
 use std::collections::HashMap;
 use std::io::Write;
 use std::ops::Range;
+use std::sync::Arc;
 
 /// The type of the step: hypothesis, normal proof step of QED step.
 enum StepType {
@@ -28,14 +33,43 @@ struct Step {
     formula: Option<Formula>,
 }
 
-#[derive(Default)]
+/// A Position information within the proof file
+pub struct Span {
+    pub line_start: usize,
+    pub col_start: usize,
+    pub line_end: usize,
+    pub col_end: usize,
+}
+
+impl Span {
+    fn new(line_start: usize, col_start: usize, slice: RopeSlice) -> Self {
+        let len_lines = slice.len_lines();
+        Span {
+            line_start,
+            col_start,
+            line_end: line_start + len_lines,
+            col_end: slice.len_chars() - slice.line_to_char(len_lines) 
+        }
+    }
+}
+
+/// A Diagnostic
+pub enum Diag {
+    UnparseableFirstLine(Span),
+    UnparseableProofLine(Span),
+    DatabaseDiagnostic(Diagnostic),
+}
+
+#[derive(Default, Clone)]
 pub struct ProofWorksheet {
     source: Rope,
     db: Database,
+    diags: Arc<HashMap<usize, Vec<Diag>>>,
     sadd: Option<StatementAddress>,
     loc_after: Option<StatementAddress>,
-    steps_by_name: HashMap<String, Step>,
-    steps_by_line: HashMap<usize, String>,
+    steps: Arc<Tree<Step>>,
+    steps_by_name: Arc<HashMap<String, NodeId>>,
+    steps_by_line: Arc<HashMap<usize, String>>,
 }
 
 /// Checks whether the line is a follow-up of the previous line
@@ -60,6 +94,10 @@ impl ProofWorksheet {
         worksheet
     }
 
+    pub fn diag(&mut self, line: usize, diag: Diag) {
+        self.diags.entry(line).or_insert_with(Vec::new).push(diag);
+    }
+
     // First line has changed, update theorem name, loc_after
     fn update_first_line(&mut self) {
         lazy_static! {
@@ -81,8 +119,8 @@ impl ProofWorksheet {
                     .map(StatementRef::address);
             }
             None => {
-                // TODO - Diagnostics!
                 error!("Could not parse first line!");
+                self.diag(0, Diag::UnparseableFirstLine(Span::new(0, 0, self.source.line(0))));
             }
         }
     }
@@ -116,9 +154,12 @@ impl ProofWorksheet {
             line_nums.end += 1;
         }
 
+        // Remove the steps at the previous lines, and the associated errors
+        // TODO
+
         // Actually attempt to parse the new lines
-        let nset = self.db.name_result();
-        let grammar = self.db.grammar_result();
+        let nset = self.db.name_result().clone();
+        let grammar = self.db.grammar_result().clone();
         let _provable = grammar.provable_typecode();
         let logic = nset.lookup_symbol(b"wff").unwrap().atom; // TODO - need metamath-knife API to get grammar's logic type.
         let mut current_line = vec![];
@@ -152,19 +193,25 @@ impl ProofWorksheet {
                         let mut symbols = formula_string.trim().split(' ');
                         let _typecode = symbols.next();
                         // TODO - Check typecode is provable
-                        let formula = grammar
-                            .parse_formula(
-                                &mut symbols
-                                    .map(|t| nset.lookup_symbol(t.as_bytes()).unwrap().atom),
-                                &[logic],
-                                nset,
-                            )
-                            .ok();
-                        info!(
-                            "Matched line {}, formula {}",
-                            line_num,
-                            formula.clone().unwrap().as_ref(&self.db)
-                        );
+                        let formula = match grammar.parse_formula(
+                            &mut symbols
+                                .map(|t| nset.lookup_symbol(t.as_bytes()).unwrap().atom),
+                            &[logic],
+                            &nset,
+                        ) {
+                            Ok(formula) => {
+                                info!(
+                                    "Matched line {}, formula {}",
+                                    line_num,
+                                    formula.clone().as_ref(&self.db)
+                                );
+                                Some(formula)
+                            },
+                            Err(diag) => {
+                                self.diag(line_num, Diag::DatabaseDiagnostic(diag));
+                                None
+                            },
+                        };
                         let step = Step {
                             name: step_name.to_string(),
                             step_type: StepType::Step,
@@ -178,8 +225,8 @@ impl ProofWorksheet {
                         self.steps_by_name.insert(step_name.to_string(), step);
                     }
                     None => {
-                        // TODO - Diagnostics!
                         error!("Could not parse line {}", as_str(&current_line));
+                        self.diag(line_num, Diag::UnparseableProofLine(Span::new(0, 0, self.source.line(0))));
                     }
                 }
                 current_line = vec![]; // TODO find how to empty
