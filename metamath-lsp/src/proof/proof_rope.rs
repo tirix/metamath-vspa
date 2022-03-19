12ea272392a1687a91f3cbf8c1c125f41b6725ec
Thierry Arnoux <thierry.arnoux@gmx.net>
2022-03-19 14:59:23 +0800

    Improved ropes for proof files

--- metamath-lsp/src/proof/proof_rope.rs
+++ metamath-lsp/src/proof/proof_rope.rs
@@ -1,6 +1,8 @@
 //! This is a xi-rope data structure, which also holds information about proof step_starts
 //  This is heavily based on xi_rope::Rope
 use std::borrow::Cow;
+use std::ops::Bound;
+use std::ops::RangeBounds;
 use std::str::FromStr;
 use std::string::ParseError;
 
@@ -18,7 +20,7 @@ use xi_rope::Interval;
 use xi_rope::{Cursor, Delta, Metric};
 
 /// Structure for a rope containing proof information
-/// This structure allows to retrieve line number information, but also step number.
+/// This structure allows to retrieve not only line number information, but also step number.
 #[derive(Clone, Default)]
 pub struct ProofRope(Node<StepsInfo>);
 
@@ -30,6 +32,11 @@ impl ProofRope {
 
     /// Apply the given transformation to this proof
     pub fn apply(&self, delta: ProofDelta) -> Self {
+        log::debug!("Char count: {} / Line count: {} / Steps count: {}", 
+            self.0.measure::<BaseMetric>(), 
+            self.0.measure::<LinesMetric>(),
+            self.0.measure::<StepsMetric>(),
+        );
         ProofRope(delta.0.apply(&self.0))
     }
 }
@@ -127,14 +134,16 @@ const MIN_LEAF: usize = 511;
 const MAX_LEAF: usize = 1024;
 
 /// This `StepsInfo` structure is used as nodes in the `ProofRope`.
-/// It stores both the number of lines and the number of step_starts at each node.
-///
-/// It is associated with the `StepsLeaf` leaf structure, which holds
-/// the string at the leaf, as well as the corresponding step_starts.
+/// It stores both the number of lines and the number of step starts at each node.
+/// Note that in order to determine if a newline is a step start, 
+/// one needs to consider the character right after the newline.
+/// If it is any kind of space, or a comment, this is not a step start.
 #[derive(Clone)]
 pub struct StepsInfo {
     lines: usize,
     step_starts: usize,
+    ends_with_newline: bool,
+    possible_step_start: bool,
 }
 
 impl NodeInfo for StepsInfo {
@@ -145,13 +154,20 @@ impl NodeInfo for StepsInfo {
     fn accumulate(&mut self, other: &Self) {
         self.lines += other.lines;
         self.step_starts += other.step_starts;
+        self.ends_with_newline = other.ends_with_newline;
+        self.possible_step_start = self.possible_step_start;
+        if self.ends_with_newline && other.possible_step_start {
+            self.step_starts += 1;
+        }
     }
 
     /// Initial calculations for a leaf.
     fn compute_info(l: &Self::L) -> Self {
         StepsInfo {
             lines: count_newlines(&l.text),
-            step_starts: l.step_starts.len(),
+            step_starts: l.count_step_starts(..),
+            ends_with_newline: l.ends_with_newline(),
+            possible_step_start: l.possible_step_start(),
         }
     }
 }
@@ -163,7 +179,7 @@ impl DefaultMetric for StepsInfo {
 /// If there is any space character at the beginning of a line,
 /// it is a follow-up of the previous line, and belongs to the same step.
 fn is_followup_char(c: u8) -> bool {
-    c == b' ' || c == b'\t' || c == b'\n'
+    c == b' ' || c == b'\t' || c == b'\n' || c == b'*'
 }
 
 fn find_leaf_split_for_bulk(s: &str) -> usize {
@@ -174,48 +190,81 @@ fn find_leaf_split_for_merge(s: &str) -> usize {
     find_leaf_split(s, max(MIN_LEAF, s.len() - MAX_LEAF))
 }
 
-// Try to split at the first possible step boundary, if not, then split at codepoint
+// Try to split before the first possible step boundary
 fn find_leaf_split(s: &str, minsplit: usize) -> usize {
     let splitpoint = min(MAX_LEAF, s.len() - MIN_LEAF);
     let bytes = s.as_bytes();
-    let mut offset = minsplit - 1;
-    while let Some(pos) = memrchr(b'\n', &bytes[offset..splitpoint]) {
-        offset += pos + 1;
+    let mut offset = minsplit;
+    loop {
+        if let Some(pos) = memrchr(b'\n', &bytes[offset..splitpoint]) {
+            offset += pos + 1;
+            if !is_followup_char(bytes[offset]) {
+                break;
+            }
+        }
+        else {
+            break;
+        }
     }
     offset
 }
 
-/// This step_starts leaf stores 2 kinds of information:
+/// A
 #[derive(Clone, Default)]
 pub struct StepsLeaf {
     /// content of this leaf
     text: String,
-    /// step_starts
-    step_starts: Vec<usize>,
+}
+
+impl StepsLeaf {
+    fn from_str_inner(s: &str) -> Self {
+        Self {
+            text: s.to_string(),
+        }
+    }
+
+    fn ends_with_newline(&self) -> bool {
+        self.text.len() == 0 || self.text.as_bytes()[self.text.as_bytes().len()-1] == b'\n'
+    }
+    
+    fn possible_step_start(&self) -> bool {
+        self.text.len() == 0 || !is_followup_char(self.text.as_bytes()[0])
+    }
+
+    fn count_step_starts<R>(&self, range: R) -> usize where R: RangeBounds<usize> {
+        let mut offset = match range.start_bound() {
+            Bound::Included(start) => *start,
+            Bound::Excluded(start) => *start - 1,
+            Bound::Unbounded => 0,
+        };
+        let end = match range.end_bound() {
+            Bound::Included(end) => *end + 1,
+            Bound::Excluded(end) => *end,
+            Bound::Unbounded => self.text.len(),
+        };
+        let mut count = 0;
+        loop {
+            match memchr(b'\n', &self.text.as_bytes()[offset..]) {
+                Some(pos) => {
+                    offset += pos + 1;
+                    if offset >= end { break; }
+                    if !is_followup_char(self.text.as_bytes()[offset]) { count += 1; }
+                },
+                _ => break,
+            }
+        }
+        count
+    }
 }
 
 impl From<&str> for StepsLeaf {
     fn from(s: &str) -> Self {
-        let mut step_starts = vec![];
-        let mut offset = 0;
-        let bytes = s.as_bytes();
-        while offset < s.len() {
-            if let Some(pos) = memchr(b'\n', &bytes[offset..]) {
-                offset += pos + 1;
-                if offset < bytes.len() && !is_followup_char(bytes[offset]) {
-                    step_starts.push(offset);
-                }
-            } else {
-                break;
-            }
-        }
-        Self {
-            text: s.to_owned(),
-            step_starts,
+        StepsLeaf {
+            text: s.to_string(),
         }
     }
 }
-
+        
 impl Leaf for StepsLeaf {
     fn len(&self) -> usize {
         self.text.len()
@@ -226,30 +275,18 @@ impl Leaf for StepsLeaf {
     }
 
     fn push_maybe_split(&mut self, other: &StepsLeaf, iv: Interval) -> Option<StepsLeaf> {
-        //println!("push_maybe_split [{}] [{}] {:?}", self, other, iv);
+        log::debug!("push_maybe_split [{:?}] [{:?}] {:?}", self, other, iv);
         let (start, end) = iv.start_end();
-        let len = self.len();
         self.text.push_str(&other.text[start..end]);
-        for &v in &other.step_starts {
-            if start < v && v <= end {
-                self.step_starts.push(v - start + len);
-            }
-        }
         if self.text.len() <= MAX_LEAF {
             None
         } else {
             let splitpoint = find_leaf_split_for_merge(&self.text);
             let right_str = self.text[splitpoint..].to_owned();
-            let split_index = self
-                .step_starts
-                .iter()
-                .position(|index| *index > splitpoint)
-                .unwrap_or(0);
             self.text.truncate(splitpoint);
             self.text.shrink_to_fit();
             Some(StepsLeaf {
                 text: right_str,
-                step_starts: self.step_starts.split_off(split_index),
             })
         }
     }
@@ -260,6 +297,24 @@ impl Leaf for StepsLeaf {
             panic!("unexpected split");
         }
         result
+    }
+}
+
+impl std::fmt::Debug for StepsLeaf {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("leaf")
+            .field("text", &self.text)
+            // .field("step_first_8_chars", 
+            //     &self.step_starts.iter().map(|index| {
+            //         if *index > self.text.len() { 
+            //             (*index, "outside of leaf!!!")
+            //         } else {
+            //             let end = if index+8 > self.text.len() { self.text.len() } else { index+8 };
+            //             (*index, &self.text[*index..end])
+            //         }
+            //     }).collect::<Vec<(usize, &str)>>()
+            //  )
+        .finish()
     }
 }
 
@@ -290,12 +345,6 @@ impl ProofDelta {
 /// its _base unit_ are utf8 code unit.
 #[derive(Clone, Copy)]
 pub struct BaseMetric(usize); // number of chars
-
-#[derive(Clone, Copy)]
-pub struct LinesMetric(usize); // number of lines
-
-#[derive(Clone, Copy)]
-pub struct StepsMetric(usize); // number of step_starts
 
 impl Metric<StepsInfo> for BaseMetric {
     fn measure(_: &StepsInfo, len: usize) -> usize {
@@ -349,6 +398,9 @@ impl Metric<StepsInfo> for BaseMetric {
 /// Measured unit is newline amount.
 /// Base unit is byte.
 /// Boundary is trailing and determined by a newline char.
+#[derive(Clone, Copy)]
+pub struct LinesMetric(usize); // number of lines
+
 impl Metric<StepsInfo> for LinesMetric {
     fn measure(info: &StepsInfo, _: usize) -> usize {
         info.lines
@@ -392,73 +444,78 @@ impl Metric<StepsInfo> for LinesMetric {
     }
 }
 
-/// Measured unit is newline amount.
+/// Measured unit is proof step.
 /// Base unit is utf8 code unit.
-/// Boundary is trailing and determined by a newline char.
+/// Boundary is a newline char, followed by a non-followup character.
+#[derive(Clone, Copy)]
+pub struct StepsMetric(usize); // number of step_starts
+
 impl Metric<StepsInfo> for StepsMetric {
-    fn measure(info: &StepsInfo, _: usize) -> usize {
+    fn measure(info: &StepsInfo, _len: usize) -> usize {
         info.step_starts
     }
 
-    ///
     fn is_boundary(s: &StepsLeaf, offset: usize) -> bool {
         if offset == 0 {
             // shouldn't be called with this, but be defensive
             false
         } else {
-            s.text.as_bytes()[offset - 1] == b'\n' && !is_followup_char(s.text.as_bytes()[offset])
+            s.text.as_bytes()[offset - 1] == b'\n'
+            && !is_followup_char(s.text.as_bytes()[offset])
         }
     }
 
-    /// Returns the smallest offset, in base units, for an offset in step_starts.
-    fn to_base_units(l: &StepsLeaf, in_measured_units: usize) -> usize {
-        if in_measured_units > l.step_starts.len() {
-            l.len() + 1
-        } else if in_measured_units == 0 {
-            0
-        } else {
-            l.step_starts[in_measured_units - 1]
-        }
-    }
-
-    /// Counts how many step starts there are in the given string
-    fn from_base_units(l: &StepsLeaf, in_base_units: usize) -> usize {
-        match l.step_starts.binary_search(&in_base_units) {
-            Ok(n) => n + 1,
-            Err(n) => n,
-        }
-    }
-
-    fn prev(l: &StepsLeaf, offset: usize) -> Option<usize> {
-        for i in 0..l.step_starts.len() {
-            if offset <= l.step_starts[i] {
-                if i == 0 {
-                    return None;
-                } else {
-                    return Some(l.step_starts[i - 1]);
-                }
+    fn to_base_units(s: &StepsLeaf, in_measured_units: usize) -> usize {
+        let mut offset = 0;
+        let mut index = 0;
+        loop {
+            match memchr(b'\n', &s.text.as_bytes()[offset..]) {
+                Some(pos) => {
+                    offset += pos + 1;
+                    if !is_followup_char(s.text.as_bytes()[offset]) { index += 1; }
+                    if index == in_measured_units { break; }
+                },
+                _ => panic!("to_base_units called with arg too large"),
             }
         }
-        l.step_starts.last().cloned()
+        offset
     }
 
-    fn next(l: &StepsLeaf, offset: usize) -> Option<usize> {
-        let n = match l.step_starts.binary_search(&offset) {
-            Ok(n) => n + 1,
-            Err(n) => n,
-        };
+    fn from_base_units(s: &StepsLeaf, in_base_units: usize) -> usize {
+        s.count_step_starts(0..in_base_units)
+    }
 
-        if n == l.step_starts.len() {
-            None
-        } else {
-            Some(l.step_starts[n])
+    fn prev(s: &StepsLeaf, offset: usize) -> Option<usize> {
+        debug_assert!(offset > 0, "caller is responsible for validating input");
+        let pos = offset;
+        loop {
+            if let Some(pos) = memrchr(b'\n', &s.text.as_bytes()[..pos - 1]) {
+                if !is_followup_char(s.text.as_bytes()[pos + 1]) { break; }
+            } else {
+                return None;
+            }
         }
+        Some(pos + 1)
+    }
+
+    fn next(s: &StepsLeaf, offset: usize) -> Option<usize> {
+        debug_assert!(offset < s.len(), "caller is responsible for validating input");
+        let pos = offset;
+        loop {
+            if let Some(pos) = memrchr(b'\n', &s.text.as_bytes()[pos..]) {
+                if !is_followup_char(s.text.as_bytes()[pos + 1]) { break; }
+            } else {
+                return None;
+            }
+        }
+        Some(pos + 1)
     }
 
     fn can_fragment() -> bool {
         true
     }
 }
+
 
 impl StringTreeBuilder for TreeBuilder<StepsInfo> {
     /// Push a string on the accumulating tree in the naive way.
@@ -512,8 +569,8 @@ mod tests {
         let p = ProofRope::from_str("a").unwrap();
         assert_eq!(1, p.char_len());
         assert_eq!(1, p.0.measure::<BaseMetric>());
-        assert_eq!(0, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(0, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(0, p.0.measure::<LinesMetric>());
+        assert_eq!(0, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -521,8 +578,8 @@ mod tests {
         let p = ProofRope::from_str("a\n").unwrap();
         assert_eq!(2, p.char_len());
         assert_eq!(2, p.0.measure::<BaseMetric>());
-        assert_eq!(1, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(0, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(1, p.0.measure::<LinesMetric>());
+        assert_eq!(0, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -530,8 +587,8 @@ mod tests {
         let p = ProofRope::from_str("a\nb").unwrap();
         assert_eq!(3, p.char_len());
         assert_eq!(3, p.0.measure::<BaseMetric>());
-        assert_eq!(1, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(1, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(1, p.0.measure::<LinesMetric>());
+        assert_eq!(1, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -539,8 +596,8 @@ mod tests {
         let p = ProofRope::from_str("a\n ").unwrap();
         assert_eq!(3, p.char_len());
         assert_eq!(3, p.0.measure::<BaseMetric>());
-        assert_eq!(1, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(0, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(1, p.0.measure::<LinesMetric>());
+        assert_eq!(0, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -548,8 +605,8 @@ mod tests {
         let p = ProofRope::from_str("a\n \n ").unwrap();
         assert_eq!(5, p.char_len());
         assert_eq!(5, p.0.measure::<BaseMetric>());
-        assert_eq!(2, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(0, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(2, p.0.measure::<LinesMetric>());
+        assert_eq!(0, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -560,8 +617,8 @@ mod tests {
         let p = ProofRope::from_str(&long_text).unwrap();
         assert_eq!(64 * 33, p.char_len());
         assert_eq!(64 * 33, p.0.measure::<BaseMetric>());
-        assert_eq!(0, p.0.measure::<LinesMetric>()); // Fix that!
-        assert_eq!(0, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(0, p.0.measure::<LinesMetric>());
+        assert_eq!(0, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -571,7 +628,7 @@ mod tests {
         assert_eq!(2000, p.char_len());
         assert_eq!(2000, p.0.measure::<BaseMetric>());
         assert_eq!(1000, p.0.measure::<LinesMetric>());
-        assert_eq!(998, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(999, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -581,7 +638,7 @@ mod tests {
         assert_eq!(4000, p.char_len());
         assert_eq!(4000, p.0.measure::<BaseMetric>());
         assert_eq!(2000, p.0.measure::<LinesMetric>());
-        assert_eq!(996, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(999, p.0.measure::<StepsMetric>());
     }
 
     #[test]
@@ -626,11 +683,11 @@ mod tests {
 
     #[test]
     fn change_insert() {
-        let p = ProofRope::from_str(&"1000:: \n".repeat(1000)).unwrap();
-        assert_eq!(8000, p.char_len());
-        assert_eq!(8000, p.0.measure::<BaseMetric>());
-        assert_eq!(1000, p.0.measure::<LinesMetric>());
-        assert_eq!(992, p.0.measure::<StepsMetric>()); // Fix that!
+        let p = ProofRope::from_str(&"1000:: \n".repeat(3)).unwrap();
+        assert_eq!(24, p.char_len());
+        assert_eq!(24, p.0.measure::<BaseMetric>());
+        assert_eq!(3, p.0.measure::<LinesMetric>());
+        assert_eq!(2, p.0.measure::<StepsMetric>());
         let change = TextDocumentContentChangeEvent {
             range: Some(Range {
                 start: Position {
@@ -647,19 +704,48 @@ mod tests {
         };
         let delta = ProofDelta::from_lsp_change(&p, &change).unwrap();
         let p = p.apply(delta);
-        assert_eq!(8008, p.char_len());
-        assert_eq!(8008, p.0.measure::<BaseMetric>());
-        assert_eq!(1000, p.0.measure::<LinesMetric>());
-        assert_eq!(992, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(32, p.char_len());
+        assert_eq!(32, p.0.measure::<BaseMetric>());
+        assert_eq!(3, p.0.measure::<LinesMetric>());
+        assert_eq!(2, p.0.measure::<StepsMetric>());
     }
 
     #[test]
     fn change_insert_new_steps() {
+        let p = ProofRope::from_str(&"1000:: \n".repeat(3)).unwrap();
+        assert_eq!(24, p.char_len());
+        assert_eq!(24, p.0.measure::<BaseMetric>());
+        assert_eq!(3, p.0.measure::<LinesMetric>());
+        assert_eq!(2, p.0.measure::<StepsMetric>()); // Fix that!
+        let change = TextDocumentContentChangeEvent {
+            range: Some(Range {
+                start: Position {
+                    line: 2,
+                    character: 2,
+                },
+                end: Position {
+                    line: 2,
+                    character: 2,
+                },
+            }),
+            range_length: None,
+            text: "200\n300\n".to_string(),
+        };
+        let delta = ProofDelta::from_lsp_change(&p, &change).unwrap();
+        let p = p.apply(delta);
+        assert_eq!(32, p.char_len());
+        assert_eq!(32, p.0.measure::<BaseMetric>());
+        assert_eq!(5, p.0.measure::<LinesMetric>());
+        assert_eq!(4, p.0.measure::<StepsMetric>());
+    }
+
+    #[test]
+    fn change_insert_new_steps_long() {
         let p = ProofRope::from_str(&"1000:: \n".repeat(1000)).unwrap();
         assert_eq!(8000, p.char_len());
         assert_eq!(8000, p.0.measure::<BaseMetric>());
         assert_eq!(1000, p.0.measure::<LinesMetric>());
-        assert_eq!(992, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(999, p.0.measure::<StepsMetric>());
         let change = TextDocumentContentChangeEvent {
             range: Some(Range {
                 start: Position {
@@ -679,6 +765,6 @@ mod tests {
         assert_eq!(8008, p.char_len());
         assert_eq!(8008, p.0.measure::<BaseMetric>());
         assert_eq!(1002, p.0.measure::<LinesMetric>());
-        assert_eq!(993, p.0.measure::<StepsMetric>()); // Fix that!
+        assert_eq!(1001, p.0.measure::<StepsMetric>());
     }
 }
